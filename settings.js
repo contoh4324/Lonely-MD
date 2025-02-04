3e931592af5b48f6ed4b245d6244331029a5dd49
contoh4324 <krsnwldn@gmail.com>
2025-02-04 21:52:24 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'lonely',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Lonely Bot",
+  botOwner: 'lonely', // Your name
+  ownerNumber: '6285876648286', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
